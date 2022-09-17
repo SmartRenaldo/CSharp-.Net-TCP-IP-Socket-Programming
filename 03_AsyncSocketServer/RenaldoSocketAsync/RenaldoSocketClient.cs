acc6c2d3eab3b4332ab8df2a5ec410dd57d2d24c
correctgavin@gmail.com <correctgavin@gmail.com>
2022-09-17 21:48:23 +0930

    async socket client

--- 03_AsyncSocketServer/RenaldoSocketAsync/RenaldoSocketClient.cs
+++ 03_AsyncSocketServer/RenaldoSocketAsync/RenaldoSocketClient.cs
@@ -1,12 +1,84 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace RenaldoSocketAsync
 {
-    internal class RenaldoSocketClient
+    public class RenaldoSocketClient
     {
+        IPAddress serverIpAddress;
+        int serverPort;
+        TcpClient TcpClient;
+
+        public RenaldoSocketClient()
+        {
+            this.serverIpAddress = null;
+            this.serverPort = -1;
+            this.TcpClient = null;
+        }
+
+        public IPAddress ServerIpAddress
+        {
+            get { return this.serverIpAddress; }
+        }
+
+        public int ServerPort
+        {
+            get { return this.serverPort; }
+        }
+
+        public bool SetServerIpAddress(string _IpAddressServer)
+        {
+            IPAddress _IpAddress = null;
+
+            if (!IPAddress.TryParse(_IpAddressServer, out _IpAddress))
+            {
+                Console.WriteLine("Invalid server IP supplied...");
+
+                return false;
+            }
+
+            serverIpAddress = _IpAddress;
+
+            return true;
+        }
+
+        public bool SetPort(string _ServerPort)
+        {
+            int portNum = 0;
+
+            if(!int.TryParse(_ServerPort, out portNum) || portNum < 0 || portNum > 65535)
+            {
+                Console.WriteLine("Invalid port number supplied...");
+
+                return false;
+            }
+
+            serverPort = portNum;
+
+            return true;
+        }
+
+        public async Task ConnectToServer()
+        {
+            if(TcpClient == null)
+            {
+                TcpClient = new TcpClient();
+            }
+
+            try
+            {
+                await TcpClient.ConnectAsync(ServerIpAddress, ServerPort);
+                Console.WriteLine(string.Format("Connected to server IP / PORT: {0} / {1}", serverIpAddress, serverPort));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
     }
 }
--- 04_AsyncSocketClient/04_AsyncSocketClient/Program.cs
+++ 04_AsyncSocketClient/04_AsyncSocketClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RenaldoSocketAsync;
 
 namespace _04_AsyncSocketClient
 {
@@ -10,6 +11,31 @@ namespace _04_AsyncSocketClient
     {
         static void Main(string[] args)
         {
+            RenaldoSocketClient client = new RenaldoSocketClient();
+            Console.WriteLine("Please type a valid server IP address and press enter:");
+
+            string strIpAddress = Console.ReadLine();
+
+            Console.WriteLine("Please supply a valid port number 0 - 65536 and press enter:");
+            string strPort = Console.ReadLine();
+
+            if (!client.SetServerIpAddress(strIpAddress) || !client.SetPort(strPort))
+            {
+                Console.WriteLine(string.Format("Wrong IP address ot port number supplied - {0} - {1} ",
+                    strIpAddress, strPort));
+                Console.ReadKey();
+
+                return;
+            }
+
+            client.ConnectToServer();
+
+            string userInput = null;
+
+            do
+            {
+                userInput = Console.ReadLine();
+            } while (!userInput.Equals("EXIT"));
         }
     }
 }
